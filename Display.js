98aa5626b2c60ff9a7ed37fe2ab8b42e34cfaa33
Matias Godoy <matiasjuliangodoy@gmail.com>
2021-10-17 20:33:57 -0300

    toFixed for decimals is 2

--- Display.js
+++ Display.js
@@ -39,11 +39,19 @@ class Display {
     const valAnterior = parseFloat(this.valorAnterior);
     const valActual = parseFloat(this.valorActual);
     if (isNaN(valActual) || isNaN(valAnterior)) return;
-    this.valorActual = this.calculator[this.operacion](
-      valAnterior,
-      valActual
-    ).toFixed(4);
-    console.log(this.valorActual);
+    if (
+      Number.isInteger(this.calculator[this.operacion](valAnterior, valActual))
+    ) {
+      this.valorActual = this.calculator[this.operacion](
+        valAnterior,
+        valActual
+      );
+    } else {
+      this.valorActual = this.calculator[this.operacion](
+        valAnterior,
+        valActual
+      ).toFixed(2);
+    }
   }
   computar(operation) {
     this.operacion !== "igual" && this.calcular();
